b2a3fb8496c682e64db6228b80aaa2be8268aed2
wesley-lewis <lewis.wes.23@gmail.com>
2023-11-15 01:20:55 +0530

    File & FileStorage added

--- models/models.go
+++ models/models.go
@@ -4,4 +4,14 @@ type UserData struct {
 	Name      string `json:"name"`
 	Mobile_no string `json:"mobile_no"`
 	Email     string `json:"email"`
+	Files     []File
+}
+
+type FileStorage struct {
+	Dir map[string]string
+}
+
+type File struct {
+	Id       string
+	Filename string
 }
