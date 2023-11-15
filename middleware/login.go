0da69645430837dd3b5b418a1ffa805a70586499
wesley-lewis <lewis.wes.23@gmail.com>
2023-11-16 02:03:43 +0530

    template for IsLoggedIn & IsRegistered

---
+++ middleware/login.go
@@ -0,0 +1,11 @@
+package middleware
+
+// IsLoggedIn will check whether the user has logged in or not.
+// In technical terms the user could provide a JWT, or some token in the request. This token will be checked whether its valid or not and accordingly the user will be allowed access to the route/resource
+func IsLoggedIn() {
+	// TODO:
+}
+
+func IsRegistered() {
+	// TODO:
+}
